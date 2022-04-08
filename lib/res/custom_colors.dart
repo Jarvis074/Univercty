3578d7287ee63a49f1b3c0f44c351bd50ed812b0
Vsevolod Ryzhenkov <vsevolod.ryzhenkov@yandex.ru>
2022-04-08 16:40:06 +0300

    First commit

---
+++ lib/res/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color univercityIndigo = Color(0xff8069F5);
+  static final Color firebaseAmber = Color(0xff8069F5);
+  static final Color firebaseYellow = Color(0xFFD08EFD);
+  static final Color firebaseGrey = Colors.black;
+  static final Color googleBackground = Color(0xFF4285F4);
+}
